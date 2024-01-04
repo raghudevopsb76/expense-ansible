15d9c0e29ca1fc5faf47abce554dc62df83fcfdc
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-04 07:27:34 +0530

    TF Code

---
+++ mutable.sh
@@ -0,0 +1,10 @@
+ENV=$1
+APP_VERSION=$2
+COMPONENT=$3
+
+##
+# Find the servers (Anisble Dynamic Inventory)
+
+# Change the Parameter store having the app version.
+
+# Run ansible push on the servers
