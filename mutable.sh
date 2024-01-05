878c7e1c7ee67d502adb041712fd32a5f2f855b5
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-05 07:21:46 +0530

    TF Code

--- mutable.sh
+++ mutable.sh
@@ -4,9 +4,12 @@
 
 env
 
-##
 # Find the servers (Anisble Dynamic Inventory)
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${ENV}-${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
 
 # Change the Parameter store having the app version.
+aws ssm put-parameter --name "${ENV}.${COMPONENT}.app_version" --value "${APP_VERSION}" --type "String" --overwrite
 
 # Run ansible push on the servers
+ansible-playbook -i inv expense.yml -e role_name=${COMPONENT} -e env=${ENV} -e version=${APP_VERSION}
+
