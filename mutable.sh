84732f450940f0074434a2354d43a4329c08ca2a
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-05 07:22:03 +0530

    TF Code

--- mutable.sh
+++ mutable.sh
@@ -1,9 +1,3 @@
-#ENV=$1
-#APP_VERSION=$2
-#COMPONENT=$3
-
-env
-
 # Find the servers (Anisble Dynamic Inventory)
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${ENV}-${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
 
