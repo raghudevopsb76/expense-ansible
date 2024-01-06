6108eba82a519a205c52d3229e3ff227bedc150c
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-06 06:51:59 +0530

    Exit Status Check

--- mutable.sh
+++ mutable.sh
@@ -1,6 +1,5 @@
 # Find the servers (Anisble Dynamic Inventory)
 
-exit 1
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${ENV}-${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
 
 # Change the Parameter store having the app version.
@@ -8,6 +7,6 @@ aws ssm put-parameter --name "${ENV}.${COMPONENT}.app_version" --value "${APP_VE
 
 SSH_PASSWORD=$(aws ssm get-parameter --name "ssh.password" --with-decryption --query 'Parameter.Value' --output text)
 # Run ansible push on the servers
-ansible-playbook -i inv expense.yml -e role_name=${COMPONENT} -e env=${ENV} -e version=${APP_VERSION} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD} | sed -e "s|${SSH_PASSWORD}|#######|"
+ansible-playbook -i inv expense.yml -e role_name=${COMPONENT} -e env=${ENV} -e version=${APP_VERSION} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}
 #
 
